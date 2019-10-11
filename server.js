9a3dcdd4b9aa4bb3ec3aab3a436892730fa861c0
Pedro Zelada Souza <pzelada-souza@myseneca.ca>
2019-10-11 04:38:26 -0400

    Changed submitted data

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Pedro Zelada Souza Student ID: 116427188 Date: 10-10-2019
+*  Name: Pedro Zelada Souza Student ID: 116427188 Date: 11-10-2019
 *
 *  Online (Heroku) Link: https://radiant-wave-15972.herokuapp.com/
 *
